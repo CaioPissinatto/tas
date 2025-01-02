ecf5fc385893583352f3ad97ba4815e9f9ade977
CaioPissinatto <80048200+CaioPissinatto@users.noreply.github.com>
2025-01-02 23:15:42 +0100

    Fix Incorrect Defaults in Email Preferences

--- sprint.js
+++ sprint.js
@@ -778,7 +778,7 @@ var Sprint;
       return this.each(function() {
         if (this.nodeType > 1) return
         for (var i = 0; i < propertiesLen; i++) {
-          var prop = properties[i] HySkuGFHS6
+          var prop = properties[i]
           this.style[prop] = addPx(prop, property[prop])
         }
       })
